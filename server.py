8b11673366505659f9792babc4c32f5b580c67a6
EranMani <eranmanim@gmail.com>
2025-12-23 15:29:58 +0200

    feat: Connect policy logic to server. Created history class var to contain log entries.

--- server.py
+++ server.py
@@ -4,9 +4,18 @@ The server is responsible for getting requests from the client and processing th
 
 import http.server
 import json
+import time
+from collections import UserDict, deque
+from policy import Policy
+
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     # NOTE: avoid overriding __init__ since we dont need to setup unique variables for every single request
+    # Load the brain
+    policy = Policy()
+
+    # Keep last 20 history items 
+    history = deque(maxlen=20)
 
     def do_POST(self):
         if self.path == "/mitigate":
@@ -28,6 +37,28 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                 self.send_error(400, "Missing required fields: prompt and user_id")
                 return
 
+            # Pass user prompt to the policy evaulate function
+            policy_decision = self.policy.evaluate_prompt(prompt)
+
+            # Create a new entry in the history
+            log_entry = {
+                "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+                "user_id": user_id,
+                "prompt_in": prompt,
+                "action": policy_decision["action"],
+                "reason": policy_decision["reason"]
+            }
+
+            self.history.append(log_entry)
+
+            # Build the response data for the client
+            response_data = {
+                "action": policy_decision["action"],
+                "prompt_out": policy_decision["prompt_out"],
+                "reason": policy_decision["reason"],
+                "user_id": user_id,
+            }
+
             # Fetch optional fields
             # NOTE: Use get() with default values to avoid crashing
             model = data.get("model", "gpt-4o")
@@ -42,7 +73,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             # NOTE: end_headers() tells the browser the headers are done
             self.end_headers()
             # Convert the data to a JSON string and encode it to bytes
-            self.wfile.write(json.dumps(data).encode("utf-8"))
+            self.wfile.write(json.dumps(response_data).encode("utf-8"))
         else:
             self.send_error(404, "Not Found")
 
